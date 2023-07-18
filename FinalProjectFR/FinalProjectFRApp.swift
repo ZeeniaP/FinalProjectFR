6943486e5d43303c3cdcf24336c5176ebfaa8292
ZeeniaP <zeenia0315@gmail.com>
2023-07-18 10:04:27 -0700

    Initial Commit

---
+++ FinalProjectFR/FinalProjectFRApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectFRApp.swift
+//  FinalProjectFR
+//
+//  Created by scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectFRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
